c3f0bc123a9dae033d46362a250e5d1fd65c177c
SHOVON <141106301+shopnilshovon@users.noreply.github.com>
2026-05-08 12:40:33 +0600

    Add configuration file for base URL and credentials

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('LAMIX_BASE', 'http://51.210.208.26/ints');
+
+define('AGENT_USERNAME', 'AHS_SHOVON');
+define('AGENT_PASSWORD', '509124866');
+
+define('COOKIE_FILE', __DIR__ . '/cookies.txt');
+
+?>
